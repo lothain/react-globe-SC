db4cbb723058811bfa06a58192530583b6998d68
lothain <tristanlxboyd@gmail.com>
2019-09-02 15:38:22 -0400

    Continued icon testing

--- src/hooks/useMarkers.ts
+++ src/hooks/useMarkers.ts
@@ -70,7 +70,6 @@ export default function useMarkers<T>(
 
       const color = marker.color || MARKER_DEFAULT_COLOR;
       const alphaT = new TextureLoader().load("../test_b_check.jpg")
-      const iconLoader = new SVGLoader().load("../mining-king-no-tools.svg");
       const size = sizeScale(value);
       let markerObject: InteractableObject3D;
 
@@ -93,34 +92,6 @@ export default function useMarkers<T>(
                 alphaMap: alphaT,
               });
               break;
-              case MarkerType.Mine:
-                iconLoader.load('../mining-king-no-tools.svg',
-                	// called when the resource is loaded
-	              function ( data ) {
-
-                var paths = data.paths;
-
-		for ( var i = 0; i < paths.length; i ++ ) {
-
-			var path = paths[ i ];
-
-			mesh.material = new MeshBasicMaterial( {
-				color: marker.color,
-				side: DoubleSide,
-				depthWrite: false
-			} );
-
-			var shapes = path.toShapes( true );
-
-        for ( var j = 0; j < shapes.length; j ++ ) {
-
-          var shape = shapes[ j ];
-          mesh.geometry = new ShapeBufferGeometry( shape );
-
-        }
-		}
-  });
-                break;
             case MarkerType.Dot:
             default:
               mesh.geometry = new SphereGeometry(
